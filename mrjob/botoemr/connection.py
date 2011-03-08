060266fe9a5fb49cb3e99854dfcc9962c6ea9f45
David Marin <dave@yelp.com>
2011-03-08 15:54:02 -0800

    Updated copyright notices

--- mrjob/botoemr/connection.py
+++ mrjob/botoemr/connection.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 Spotify AB
+# Copyright (c) 2010-2011 Spotify AB, Yelp
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the

6f47a7ed70a0531ec799a033e1811d83501af7a3
Steve Johnson <sjohnson@yelp.com>
2011-06-22 17:39:44 -0700

    Added s3lock which should have been in about 3 commits ago

---
+++ mrjob/s3lock.py
@@ -0,0 +1,56 @@
+# Copyright 2009-2011 Yelp and Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Facilities for "locking" EMR job flows via S3. This can be used to avoid
+race conditions where two jobs see that a job flow is available and add
+themselves to it, resulting in one job flow being scheduled behidn the other
+and probably making the developer very unhappy."""
+from __future__ import with_statement
+
+import os
+import time
+
+from mrjob.parse import parse_s3_uri
+
+
+def make_uid():
+    """Generate a string that is local to the current 'lock attempt'. Uses
+    things like host, username, and time. Could be replaced by a better GUID
+    scheme.
+    """
+    return str(os.uname()) + os.getlogin()
+
+def make_lock_uri(s3_tmp_uri, emr_job_flow_id, step_num):
+    """Generate the URI to lock the job flow ``emr_job_flow_id``"""
+    return s3_tmp_uri + 'locks/' + emr_job_flow_id + '/' + str(step_num)
+
+def attempt_to_acquire(s3_conn, lock_uri, sync_wait_time=5.0, uid=None):
+    """Returns True if this session successfully took ownership of the lock
+    specified by ``lock_uri``.
+    """
+    uid = uid or make_uid()
+    bucket_name, key_prefix = parse_s3_uri(lock_uri)
+    bucket = s3_conn.get_bucket(bucket_name)
+    key = bucket.get_key(lock_uri)
+
+    if key is None:
+        key = bucket.new_key(lock_uri)
+        key.set_contents_from_string(uid)
+        time.sleep(sync_wait_time)
+        key_value = key.get_contents_as_string()
+        if key_value == uid:
+            return True
+
+    print 'could not acquire lock for', lock_uri
+
+    return False

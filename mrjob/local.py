9871fac9dab64012e8b51ace35ebc9bd3bcb681b
David Marin <dave@yelp.com>
2011-11-29 15:58:00 -0800

    added from __future__ import with_statement

--- mrjob/local.py
+++ mrjob/local.py
@@ -11,9 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Run an MRJob locally by forking off a bunch of processes and piping
 them together. Useful for testing."""
+from __future__ import with_statement
 
 from collections import defaultdict
 import itertools

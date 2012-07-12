525cff61d443498105c39e36a2b42efe40f46d6d
Stephen Johnson <steve@steveasleep.com>
2012-07-12 16:42:20 -0700

    Finish MRJobStep init test casees and JarStep test cases

--- tests/test_step.py
+++ tests/test_step.py
@@ -4,16 +4,35 @@ try:
 except ImportError:
     from unittest import TestCase
 
+from mrjob.step import _IDENTITY_MAPPER
+from mrjob.step import JarStep
 from mrjob.step import MRJobStep
 
 
-def identity_mapper(k, v):
+# functions we don't really care about the values of
+
+
+def identity_mapper(k=None, v=None):
     yield k, v
 
 
 def identity_reducer(k, vals):
     for v in vals:
         yield k, v
+
+
+class JarStepTestCase(TestCase):
+
+    def test_all(self):
+        kwargs = {
+            'name': 'step_name',
+            'jar': 'binks.jar.jar',
+            'main_class': 'MyMainMan',
+            'step_args': ['argh', 'argh'],
+        }
+        expected = kwargs.copy()
+        expected['type'] = 'jar'
+        self.assertEqual(JarStep(**kwargs).description(0), expected)
 
 
 class MRJobStepInitTestCase(TestCase):
@@ -93,8 +112,25 @@ class MRJobStepInitTestCase(TestCase):
     def test_conflict_mapper(self):
         self._test_conflict(mapper_cmd='cat', mapper=identity_mapper)
 
-    def test_conflict_mapper_init(self):
-        self._test_conflict(mapper_cmd='cat', mapper_init=identity_mapper)
+    def test_conflict_combiner(self):
+        self._test_conflict(combiner_cmd='cat', combiner=identity_reducer)
 
-    def test_conflict_mapper_final(self):
-        self._test_conflict(mapper_cmd='cat', mapper_final=identity_mapper)
+    def test_conflict_reducer(self):
+        self._test_conflict(reducer_cmd='cat', reducer=identity_reducer)
+
+
+class MRJobStepGetItemTestCase(TestCase):
+
+    def test_get_identity_mapper(self):
+        # this is the weird behavior
+        self.assertEqual(MRJobStep(mapper_final=identity_mapper)['mapper'],
+                         _IDENTITY_MAPPER)
+
+    def test_get_regular_mapper(self):
+        # this is the normal behavior
+        self.assertEqual(MRJobStep(mapper=identity_mapper)['mapper'],
+                         identity_mapper)
+
+
+class MRJobStepRenderTestCase(TestCase):
+    pass

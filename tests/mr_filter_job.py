789e3fbafab27f677b1465cd906b3feda387d792
Steve Johnson <sjohnson@yelp.com>
2012-07-13 16:20:42 -0700

    Add missing test file

---
+++ tests/mr_filter_job.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+from mrjob.protocol import RawValueProtocol
+
+
+class FilterJob(MRJob):
+
+    INPUT_PROTOCOL = RawValueProtocol
+
+    INTERNAL_PROTOCOL = RawValueProtocol
+
+    OUTPUT_PROTOCOL = RawValueProtocol
+
+    def configure_options(self):
+        super(FilterJob, self).configure_options()
+        self.add_passthrough_option('--mapper-filter', default=None)
+        self.add_passthrough_option('--reducer-filter', default=None)
+
+    def steps(self):
+        kwargs = {}
+        if self.options.mapper_filter:
+            kwargs['mapper_filter'] = self.options.mapper_filter
+        if self.options.reducer_filter:
+            kwargs['reducer_filter'] = self.options.reducer_filter
+        return [self.mr(**kwargs)]
+
+
+if __name__ == '__main__':
+    FilterJob().run()

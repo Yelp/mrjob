3b59bbb8211e82fa258aa8b5b08cb90411a7692b
Steve Johnson <sjohnson@yelp.com>
2012-07-19 16:05:11 -0700

    Put back a comment I deleted earlier

--- tests/test_local.py
+++ tests/test_local.py
@@ -27,7 +27,7 @@ import tempfile
 
 try:
     import unittest2 as unittest
-    unittest
+    unittest  # quiet "redefinition of unused ..." warning from pyflakes
 except ImportError:
     import unittest
 

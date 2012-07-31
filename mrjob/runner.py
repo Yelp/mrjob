9b89ac23a1050f760a416d79c324fead8a22db87
Steve Johnson <sjohnson@yelp.com>
2012-07-31 14:30:03 -0700

    Moved more OptionStore test cases into test_option_store

--- mrjob/runner.py
+++ mrjob/runner.py
@@ -146,7 +146,7 @@ class RunnerOptionStore(OptionStore):
 
         if (len(self.cascading_dicts) > 2 and
             all(len(d) == 0 for d in self.cascading_dicts[2:-1]) and
-            len(conf_paths) > 0):
+            (len(conf_paths) > 0 or len(opts) == 0)):
             log.warning('No configs specified for %s runner' % alias)
 
         self.populate_values_from_cascading_dicts()

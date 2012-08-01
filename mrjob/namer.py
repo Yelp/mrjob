b0d441c9589d008034db246b93d3b1eb00e70484
David Marin <dave@yelp.com>
2012-08-01 15:31:37 -0700

    reworked logic to have separate UploadDir and SetupDir

--- mrjob/namer.py
+++ mrjob/namer.py
@@ -13,130 +13,151 @@
 # limitations under the License.
 from __future__ import with_statement
 
+import itertools
 import os.path
-from random import randint
+
+from mrjob.parse import is_uri
 
 
-class NamerBase(object):
-    """Assigns unique names to paths."""
+def name_uniquely(path, names_taken, proposed_name=None):
+    """Come up with a unique name for *path*.
 
-    def __init__(self):
-        # map from path to 'archive' or 'file'. This also keeps track
-        # of which files were added
-        self._path_to_type = {}
+    :param names_taken: a dictionary or set of names not to use.
+    :param proposed_name: name to use if it is not taken. If this is not set,
+                          we propose a name based on the filename.
 
-        # map from path to assigned name
+    If the proposed name is taken, we add a number to the end of the
+    filename, keeping the extension the same. For example:
+
+    >>> name_uniquely('foo.tar.gz', set(['foo.tar.gz']))
+    'foo-1.tar.gz'
+    """
+    if not proposed_name:
+        proposed_name = os.path.basename(path) or '_'
+
+    if proposed_name not in names_taken:
+        return proposed_name
+
+    dot_idx = proposed_name.find('.')
+    if dot_idx == -1:
+        prefix, suffix = proposed_name, ''
+    else:
+        prefix, suffix = proposed_name[:dot_idx], proposed_name[dot_idx:]
+
+    for i in itertools.count(1):
+        name = '%s-%d%s' % (prefix, i, suffix)
+        if name not in names_taken:
+            return name
+
+
+class UploadDir(object):
+
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+        self._paths = set()
         self._path_to_name = {}
-        # map from name to assign path (to track which names are taken)
-        self._name_to_path = {}
+        self._names = set()
 
-    def add_file(self, path, name=None):
-        """Add a file, optionally setting its name."""
-        self._add(path, name, 'file')
+    def add(self, path, proposed_name=None):
+        self._paths.add(path)
 
-    def add_archive(self, path, name=None):
-        """Add an archive, optionally setting its name."""
-        self._add(path, name, 'archive')
+        if proposed_name:
+            self._name(path, proposed_name)
 
-    def name(self, path):
-        """Get the name assigned to path, picking one if one isn't
-        already assigned. Names will never be ''.
+    def _name(self, path, proposed_name=None):
+        assert path in self._paths
 
-        If path hasn't been added, return None.
-        """
-        if path not in self._name_to_type:
-            return None
-
-        return self._name(path)
-
-    def archive_names(self):
-        """Get a map from archive's path to its name."""
-        return dict((path, self.name(path))
-                    for path, type in self._path_to_name.iteritems()
-                    if type == 'archive')
-
-    def file_names(self):
-        """Get a map from file's path to its name."""
-        return dict((path, self.name(path))
-                    for path, type in self._path_to_name.iteritems()
-                    if type == 'file')
-
-    def _add(self, path, name, type):
-        """Helper for :py:meth:`add_archive` and :py:meth:`add_name`."""
-        if path in self._path_to_type and self._path_to_type[path] != type:
-                raise ValueError(
-                    '%r is already assigned type %r' % (path, type))
-
-        if name:
-            self._name(path, name)
-
-        self._path_to_type[path] = type
-
-    def _name(self, path, name=None):
-        """If *path* is not already named, assign a name to it
-
-        If *name* is set, we attempt to assign that name to *path*, otherwise
-        we attempt to assign *path*'s filename. If the name is taken, we
-        prepend random hex to it (we prepend so it keeps the same file
-        extension).
-
-        Return the name assigned to *path*.
-        """
         if path in self._path_to_name:
-            current_name = self._path_to_name[path]
-            if name is None or name == current_name:
-                return current_name
-            else:
-                raise ValueError(
-                    "Can't name %r %r; it's already named %r" % (
-                        path, name, current_name))
+            return self._path_to_name[path]
 
-        if name is None:
-            name = os.path.basename(path) or '_'
-        elif name == '':
-            raise ValueError('name may not be empty')
-
-        # put in subclass
-        elif name in self._name_to_path and self._naming_is_strict():
-            raise ValueError(
-                "Can't name %r %r; already belongs to %r" % (
-                    path, name, self._name_to_path[path]))
-
-        # pick a unique name
-        suffix = name
-
-        while name in self._name_to_path:
-            name = '%08x-%s' % (randint(0, 2 ** 32 - 1), suffix)
+        name = name_uniquely(path, names_taken=self._names,
+                             proposed_name=proposed_name)
 
         self._path_to_name[path] = name
-        self._name_to_path[name] = path
+        self._names.add(name)
 
         return name
 
+    def uri(self, path):
+        if path in self._paths:
+            return self.prefix + self._name(path)
+        else:
+            return path
 
-class DistributedCacheNamer(NamerBase):
-    """Like NamerBase, except that we raise an error if we can't assign
-    a path the name it was added with.
-    """
-    def _name(self, path, name=None):
-        if (name is not None and
-            name in self._path_to_name and
-            self.name_to_path.get(path) != name):
-
-            raise ValueError(
-                "Can't name %r %r; already belongs to %r" % (
-                    path, name, self._name_to_path[path]))
-
-        return super(DistributedCacheNamer, self).name(path, name=name)
+    def path_to_uri(self):
+        return dict((path, self.uri(path))
+                    for path in self._paths)
 
 
-class UploadNamer(NamerBase):
-    """Like NamerBase, except that it's initialized
-    with a prefix which :py:meth:`name` prepends to paths' assigned names.
-    """
-    def __init__(self, prefix):
-        super(UploadNamer, self).__init__()
-        self.prefix = prefix
+class SetupDir(object):
 
-    def name(self, path, name=None):
-        return self.prefix + super(UploadNamer, self).name(path, name=name)
+    SUPPORTED_TYPES = ('archive', 'file')
+
+    def __init__(self, upload_dir=None):
+        self.upload_dir = upload_dir
+
+        self._typed_paths = set()
+        self._typed_path_to_default_name = {}
+        self._name_to_typed_path = {}
+
+    def add(self, type, path, name=None):
+        self._check_type(type)
+
+        # register this file/archive
+        self._typed_paths.add((type, path))
+
+        if name:
+            if name in self._name_to_typed_path:
+                # Name is taken! Check that it belongs to this file/archive
+                current_type, current_path = self._name_to_typed_path[name]
+
+                if (type, path) != (current_type, current_path):
+                    raise ValueError(
+                        "%s won't work because we already have %" % (
+                            self._desc(type, path, name),
+                            self._desc(current_type, current_path, name)))
+            else:
+                # Name is not taken! Reserve it
+                self._name_to_typed_path[(type, path)] = name
+                self._typed_path_to_default_name.setdefault((type, path), name)
+
+        # add to our upload dir (doing this last in case of an exception above)
+        if self.upload_dir and self._should_upload(path):
+            self.upload_dir.add(path, name=name)
+
+    def name(self, type, path):
+        self._check_type(type)
+
+        if (type, path) not in self._typed_path:
+            raise ValueError('%s was never added!' % self._desc(type, path))
+
+        if (type, path) not in self._typed_path_to_default_name:
+            name = name_uniquely(path, names_taken=self._name_to_typed_path)
+            self._name_to_typed_path[name] = (type, path)
+            self._typed_path_to_default_name[(type, path)] = name
+
+        return self._typed_path_to_default_name[(type, path)]
+
+    def name_to_path(self, type):
+        self._check_type(type)
+
+        for path_type, path in self._typed_paths:
+            if path_type == type:
+                self.name(type, path)
+
+        return dict((name, typed_path)
+                    for name, typed_path
+                    in self._name_to_typed_path.iteritems()
+                    if typed_path[0] == type)
+
+    def _check_type(self, type):
+        if not type in self.SUPPORTED_TYPES:
+            raise TypeError('bad path type %r, must be one of %s' % (
+                type, ', '.join(sorted(self.SUPPORTED_TYPES))))
+
+    def _desc(self, type, path, name=''):
+        return '%s#%s%s' % (path, name, '/' if type == 'archive' else '')
+
+    def _should_upload(self, path):
+        return is_uri(path)

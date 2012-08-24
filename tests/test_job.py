5572e32bcce3761d81ff1772b219a9d3078b8bbf
David Marin <dave@yelp.com>
2012-08-24 14:41:39 -0700

    fixed job test

--- tests/test_job.py
+++ tests/test_job.py
@@ -772,9 +772,8 @@ class FileOptionsTestCase(SandboxedTestCase):
 
         with logger_disabled('mrjob.local'):
             with mr_job.make_runner() as runner:
-                # make sure our file gets "uploaded"
-                assert [fd for fd in runner._files
-                        if fd['path'] == n_file_path]
+                # make sure our file gets placed in the working dir
+                self.assertIn(n_file_path, runner._wd_mgr.paths())
 
                 runner.run()
                 output = set()

c6df85b316344858868748a73d5767b8a24b7082
Stephen Johnson <steve@steveasleep.com>
2013-09-08 00:28:48 -0700

    Fix mr_most_used_word docstring

--- mrjob/examples/mr_most_used_word.py
+++ mrjob/examples/mr_most_used_word.py
@@ -12,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The classic MapReduce job: count the frequency of words.
-"""
+"""Determine the most used word in the input."""
 from mrjob.job import MRJob
 from mrjob.protocol import JSONValueProtocol
 import re

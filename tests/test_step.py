0d409a5850ee6ff0236f74d4d26b54e89b1f3a46
David Marin <dm@davidmarin.org>
2013-11-20 10:22:00 -0800

    added missing __future__ import. Sorry, Python 2.5!

--- tests/test_step.py
+++ tests/test_step.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tests for mrjob.step"""
+from __future__ import with_statement
+
 try:
     from unittest2 import TestCase
     TestCase  # silency pyflakes

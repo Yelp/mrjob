0a4475d81c161fdc9661f9c1c415199bfe292f36
David Marin <dm@davidmarin.org>
2013-11-27 20:55:14 -0600

    updated copyright notices

--- mrjob/examples/mr_jar_step_example.py
+++ mrjob/examples/mr_jar_step_example.py
@@ -1,3 +1,16 @@
+# Copyright 2013 David Marin
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """A simple example of linking a hadoop jar with a Python step.
 
 This example only works out-of-the-box on EMR; to make it work on Hadoop,
--- tests/mockhadoop.py
+++ tests/mockhadoop.py
@@ -1,4 +1,5 @@
 # Copyright 2009-2012 Yelp
+# Copyright 2013 Tom Arnfeld and David Marin
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

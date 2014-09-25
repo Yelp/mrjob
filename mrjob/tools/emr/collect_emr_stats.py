3a9e70f7c8883db3b1f4e51facbb5b4e2778ff59
Pai-Wei Lai <paiwei@yelp.com>
2014-09-24 17:16:15 -0700

    renaming count_emr_xxx to collect_emr_stats

--- mrjob/tools/emr/collect_emr_stats.py
+++ mrjob/tools/emr/collect_emr_stats.py
@@ -11,12 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Count the numbers of EMR active clusters and instances from current jobflows.
+"""Collect EMR stats from current jobflows.
 
 Usage::
 
-    mrjob count-emr-list-active > report
-    python -m mrjob.tools.emr.count_emr_list_active > report
+    mrjob collect-emr-stats > report
+    python -m mrjob.tools.emr.collect_emr_status > report
 
 Options::
 
@@ -37,17 +37,10 @@ from optparse import OptionParser
 from mrjob.emr import EMRJobRunner
 from mrjob.emr import describe_all_job_flows
 from mrjob.job import MRJob
-#from mrjob.options import add_basic_opts
-#from mrjob.parse import JOB_NAME_RE
-#from mrjob.parse import STEP_NAME_RE
-#from mrjob.parse import iso8601_to_datetime
-#from mrjob.util import strip_microseconds
-#from mrjob.tools.emr.audit_usage import job_flow_to_basic_summary
 
 import logging
 
 log = logging.getLogger(__name__)
-
 
 def main(args):
     # parser command-line args
@@ -61,40 +54,33 @@ def main(args):
 
     now = datetime.utcnow()
 
-    log.info('listing all active emr jobflows...')
+    log.info('collecting emr jobflow information...')
 
     active_states = ['STARTING', 'BOOTSTRAPPING', 'WAITING', 'RUNNING']
-#    active_states = ['WAITING']
     emr_conn = EMRJobRunner(conf_paths=options.conf_paths).make_emr_conn()
     job_flows = describe_all_job_flows(emr_conn, states=active_states)
 
-    import subprocess
-#    subprocess.call(['emr --list --state active \| grep \'j-\''], shell=True)
-    subprocess.call(['./count.sh'], shell=True)
-
-    log.info('compiling active emr jobflows stats...')
+    log.info('compiling stats from emr jobflows...')
 
     job_flow_ids = [getattr(jf, 'jobflowid', None) for jf in job_flows]
     instance_counts = [int(getattr(jf, 'instancecount', None)) for jf in job_flows]
-   
-#    print job_flow_ids 
-#    print instance_counts
     total_instance_count = sum(instance_counts)
 
-    result =  'timestamp:' + repr(now) + 'emr_instances:' + repr(total_instance_count)
-
+    result =  'timestamp:' + repr(now) 
+    result += 'emr_jobflow_num:' + repr(len(job_flow_ids))
+    result += 'emr_instances:' + repr(total_instance_count)
     print result
 
 
 def make_option_parser():
     usage = '%prog [options]'
-    description = 'Count EMR active jobflows now, identical to \'emr --list --active\'.'
+    description = 'Collect EMR stats from current jobflows.'
     option_parser = OptionParser(usage=usage, description=description)
 #    option_parser.add_option(
 #        '--max-days-ago', dest='max_days_ago', type='float', default=None,
 #        help=('Max number of days ago to look at jobs. By default, we go back'
 #              ' as far as EMR supports (currently about 2 months)'))
-    add_basic_opts(option_parser)
+#    add_basic_opts(option_parser)
     return option_parser
 
 if __name__ == '__main__':

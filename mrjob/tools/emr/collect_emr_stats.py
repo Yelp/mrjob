d8e174a83d0cb24a18ea994e302abf77be05dbb4
Pai-Wei Lai <paiwei@yelp.com>
2014-09-30 10:32:17 -0700

    code fix after initial code review

--- mrjob/tools/emr/collect_emr_stats.py
+++ mrjob/tools/emr/collect_emr_stats.py
@@ -11,7 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Collect EMR stats from active jobflows.
+""" Collect EMR stats from active jobflows. 
+    Active jobflows are those in states of BOOTSTRAPPING, RUNNING, STARTING, 
+    and WAITING. 
+    Collected stats include total number of active jobflows and total 
+    number of instance counts from these jobflows.
 
 Usage::
 
@@ -20,28 +24,27 @@ Usage::
 
 Options::
 
-  -h, --help            show this help message and exit
-  -v, --verbose         print more messages to stderr
+  -h, --help            Show this help message and exit
+  -v, --verbose         Print more messages to stderr
   -q, --quiet           Don't log status messages; just print the report.
   -c CONF_PATH, --conf-path=CONF_PATH
                         Path to alternate mrjob.conf file to read from
   --no-conf             Don't load mrjob.conf even if it's available
 
 """
-from __future__ import with_statement
 
 from datetime import datetime
-from time import mktime
+from logging import getLogger
 from optparse import OptionParser
+from time import mktime
 
 from mrjob.emr import EMRJobRunner
 from mrjob.emr import describe_all_job_flows
 from mrjob.job import MRJob
 from mrjob.options import add_basic_opts
 
-import logging
 
-log = logging.getLogger(__name__)
+log = getLogger(__name__)
 
 def main(args):
     # parser command-line args
@@ -53,16 +56,14 @@ def main(args):
     if args:
         option_parser.error('takes no arguments')
 
-    now = datetime.utcnow()
-
     MRJob.set_up_logging(quiet=options.quiet, verbose=options.verbose)
-    log.info('collecting emr jobflow information...')
+    log.info('collecting EMR active jobflows...')
 
     job_flows = collect_active_job_flows(options.conf_paths)
 
     log.info('compiling stats from collected jobflows...')
 
-    stats = job_flows_to_stats(job_flows, now=now)
+    stats = job_flows_to_stats(job_flows)
 
     print stats
 
@@ -70,7 +71,8 @@ def main(args):
 def collect_active_job_flows(conf_paths):
     """Collect active job flow information from EMR.
 
-    :param str conf_path: Alternate path to read :py:mod:`mrjob.conf` from, or ``False`` to ignore all config files
+    :param str conf_path: Alternate path to read :py:mod:`mrjob.conf` from, 
+                          or ``False`` to ignore all config files
     """
     emr_conn = EMRJobRunner(conf_paths=conf_paths).make_emr_conn()
     active_states = ['STARTING', 'BOOTSTRAPPING', 'WAITING', 'RUNNING']
@@ -78,28 +80,28 @@ def collect_active_job_flows(conf_paths):
     return describe_all_job_flows(emr_conn, states=active_states)
 
 
-def job_flows_to_stats(job_flows, now=None):
-    """ Compute total number of jobflows and instance count given a list of jobflows
+def job_flows_to_stats(job_flows):
+    """ Compute total number of jobflows and instance count given a list of 
+        jobflows.
     
     :param job_flows: A list of :py:class:`boto.emr.EmrObject`
-    :param now: the current UTC time, as a :py:class:`datetime.datetime`. Default to current time.
 
     Returns a dictionary with many keys, including:
 
-    * *timestamp*: The time when stats are collected (current UTC time in POSIX timestamp format)
-    * *num_jobflows*: total number of jobflows
-    * *total_instance_count*: total number of instance count from jobflows
+    * *timestamp*: The time when stats are collected (current UTC time in 
+                   POSIX timestamp, float format).
+    * *num_jobflows*: Total number of jobflows.
+    * *total_instance_count*: Total number of instance counts from jobflows.
 
     """
-    job_flow_ids = [getattr(jf, 'jobflowid', None) for jf in job_flows]
-    instance_counts = [int(getattr(jf, 'instancecount', None)) for jf in job_flows]
+    job_flow_ids = [jf.jobflowid for jf in job_flows]
+    instance_counts = [int(jf.instancecount) for jf in job_flows]
     total_instance_count = sum(instance_counts)
 
-    if now is None:
-        now = datetime.utcnow()
+    now = datetime.utcnow()
 
     stats = {}
-    stats['timestamp'] = int(mktime(now.timetuple())) # convert into POSIX timestamp
+    stats['timestamp'] = mktime(now.timetuple())  # convert into POSIX timestamp
     stats['num_jobflows'] = len(job_flow_ids)
     stats['total_instance_count'] = total_instance_count
 
@@ -108,4 +110,3 @@ def job_flows_to_stats(job_flows, now=None):
 
 if __name__ == '__main__':
     main(None)
-

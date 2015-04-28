e471e0de1da961d4744bc99e38468da76813f042
David Marin <dm@davidmarin.org>
2015-04-28 11:01:11 -0700

    use `` not ` in py2, establish that log files are parsed in binary mode

--- mrjob/py2.py
+++ mrjob/py2.py
@@ -16,21 +16,23 @@ str (i.e. unicode) in Python 3.
 These things should always be bytes:
 
 - input data files
-  - use `'b'` when opening files: `open(..., 'rb')`
-  - read data from `sys.stdin.buffer` in Python 3, not `sys.stdin`
+  - use ``'b'`` when opening files: ``open(..., 'rb')``
+  - read data from ``sys.stdin.buffer`` in Python 3, not ``sys.stdin``
+- data encoded by protocols
 - data from subprocesses (this already happens by default)
+- log files parsed by mrjob
 - file content from our filesystem interfaces.
 
-Instead of using `StringIO` to deal with these, use `io.BytesIO`.
+Instead of using ``StringIO`` to deal with these, use ``io.BytesIO``.
 
-Not that both Python 2.6+ and Python 3.3+ have the `bytes` type and
-`b''` constants built-in.
+Not that both Python 2.6+ and Python 3.3+ have the ``bytes`` type and
+``b''`` constants built-in.
 
 These things should always be strings:
 
-- streams that you print() to (e.g. sys.stdout if you mock it out)
+- streams that you print() to (e.g. ``sys.stdout`` if you mock it out)
 - streams that you log to
-- paths
+- paths on filesystem
 - arguments to commands
 - option names
 
@@ -38,46 +40,46 @@ These things are strings because it makes for simpler code:
 
 - contents of config files
 - contents of scripts output by mrjob (e.g. the setup wrapper script)
-- contents of empty files (`open(..., 'w')` is fine)
+- contents of empty files
 
-Use the `StringIO` from this module to deal with strings (it's
-`StringIO.StringIO` in Python 2 and `io.StringIO` in Python 3).
+Use the ``StringIO`` from this module to deal with strings (it's
+``StringIO.StringIO`` in Python 2 and ``io.StringIO`` in Python 3).
 
-Please use `%` for format strings and not `format()`, which is much more
+Please use ``%`` for format strings and not ``format()``, which is much more
 picky about mixing unicode and bytes.
 
-This module provides a `basestring` type so you can check if something
+This module provides a ``basestring`` type so you can check if something
 is a "string".
 
-We don't provide a `unicode` constant:
+We don't provide a ``unicode`` constant:
 
-- Use `not isinstance(..., bytes)` to check if a string is Unicode
-- To convert `bytes` to `unicode`, use `.decode('utf-8')`.
-- Python 3.3+ has `u''` literals; please use sparingly
+- Use ``not isinstance(..., bytes)`` to check if a string is Unicode
+- To convert ``bytes`` to ``unicode``, use ``.decode('utf_8')`.
+- Python 3.3+ has ``u''`` literals; please use sparingly
 
 Iterables
 ---------
 
-Using `.iteritems()` or `.itervalues()` in Python 2 to iterate over a
+Using ``.iteritems()`` or ``.itervalues()`` in Python 2 to iterate over a
 dictionary when you don't need a list is best practice, but it's also (in most
-cases) an over-optimization. We'd prefer clean code; just use `.items()`
-and `.values()`.
+cases) an over-optimization. We'd prefer clean code; just use ``.items()``
+and ``.values()``.
 
-If you *do* every need that extra efficiency `for k in some_dict` does not
+If you *do* every need that extra efficiency ``for k in some_dict`` does not
 create a list in either version of Python, and you for performance-critical
-code, it's fine to write custom code for each Python version (use `IN_PY2`
+code, it's fine to write custom code for each Python version (use ``IN_PY2``
 from this module to check which version you're in).
 
-Same goes for `xrange`; just use `range` (this module provides `xrange`
-solely to support `ReprProtocol`).
+Same goes for ``xrange``; just use ``range`` (this module provides ``xrange``
+solely to support ``ReprProtocol``).
 
 Miscellany
 ----------
 
-We provide a `long` type (aliased to `int` on Python 3) so you can
-check if something is an integer: `isinstance(..., (int, long))`.
+We provide a ``long`` type (aliased to ``int`` on Python 3) so you can
+check if something is an integer: ``isinstance(..., (int, long))``.
 
-Any standard library function starting with "url" (e.g. `urlparse()`) should
+Any standard library function starting with "url" (e.g. ``urlparse()``) should
 be imported from this module.
 """
 import sys
@@ -85,21 +87,21 @@ import sys
 # use this to check if we're in Python 2
 IN_PY2 = (sys.version_info[0] == 2)
 
-# `basestring`, for `isinstance(..., basestring)`
+# ``basestring``, for ``isinstance(..., basestring)``
 if IN_PY2:
     basestring = basestring
 else:
     basestring = str
 basestring
 
-# `long`, for `isinstance(..., (int, long))`
+# ``long``, for ``isinstance(..., (int, long))``
 if IN_PY2:
     long = long
 else:
     long = int
 long
 
-# `StringIO`, for mocking out `sys.stdout`, etc. You probably won't need
+# ``StringIO``, for mocking out ``sys.stdout``, etc. You probably won't need
 # this outside of
 if IN_PY2:
     from StringIO import StringIO
@@ -114,7 +116,7 @@ else:
     from urllib.request import urlopen
 urlopen
 
-# urlparse() (in most cases you should use `mrjob.parse.urlparse()`)
+# urlparse() (in most cases you should use ``mrjob.parse.urlparse()``)
 if IN_PY2:
     from urllib import quote
     from urllib import unquote
@@ -130,9 +132,9 @@ unquote
 urlparse
 ParseResult
 
-# `xrange`, for `ReprProtocol`
+# ``xrange``, for ``ReprProtocol``
 #
-# Please just use `range` unless you really need the optimization.
+# Please just use ``range`` unless you really need the optimization.
 if IN_PY2:
     xrange = xrange
 else:

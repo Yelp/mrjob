0b8daec11ea5a29a68b30919963c4e6dd703a236
David Marin <dm@davidmarin.org>
2015-04-28 10:42:18 -0700

    require latest version of boto on Python 3

--- setup.py
+++ setup.py
@@ -33,9 +33,16 @@ try:
     # unittest2 is a backport of unittest from Python 2.7
     if sys.version_info < (2, 7):
         setuptools_kwargs['tests_require'].append('unittest2')
-    # no boto on Python 3 yet
+
+    # boto
     if sys.version_info < (3, 0):
+        # Officially, the 0.4.x series of mrjob supports boto back to v2.2.0
         setuptools_kwargs['install_requires'].append('boto>=2.2.0')
+    else:
+        # boto didn't support Python 3 until v2.32.1. Since Python 3 support
+        # is new as of mrjob v0.4.5, there's no backward compatibility issue,
+        # so we might as well require the latest version of boto.
+        setuptools_kwargs['install_requires'].append('boto>=2.38.0')
 
 except ImportError:
     from distutils.core import setup

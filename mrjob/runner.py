e00dd4cf0eeeac9415a9e13e314b62eb6a1da22e
David Marin <dm@davidmarin.org>
2015-06-25 12:05:22 -0700

    added urgency to conf_path deprecation warning

--- mrjob/runner.py
+++ mrjob/runner.py
@@ -351,8 +351,9 @@ class MRJobRunner(object):
             if conf_paths is not None:
                 raise ValueError("Can't specify both conf_path and conf_paths")
             else:
-                log.warn("The conf_path argument to MRJobRunner() is"
-                         " deprecated. Use conf_paths instead.")
+                log.warning('The conf_path argument to MRJobRunner() is'
+                            ' deprecated and will be removed in v0.5.0. Use'
+                            ' conf_paths instead.')
                 if conf_path is False:
                     conf_paths = []
                 else:

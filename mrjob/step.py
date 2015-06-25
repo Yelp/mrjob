7c114bef286747fa4e86f16b1ab006a78413b6a5
David Marin <dm@davidmarin.org>
2015-06-25 13:06:00 -0700

    removed MRJobStep (alias for MRStep)

--- mrjob/step.py
+++ mrjob/step.py
@@ -240,10 +240,6 @@ class MRStep(object):
         return substep_descs
 
 
-# for backwards compatibility; remove in v0.5.0
-MRJobStep = MRStep
-
-
 class JarStep(object):
     """Represents a running a custom Jar as a step.
 

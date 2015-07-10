6a2db24aa976a25f750bb576449ed4655d24a3c8
David Marin <dm@davidmarin.org>
2015-07-10 11:49:22 -0700

    copied code from boto 2.35.0 to support "clusters" EMR API calls

---
+++ mrjob/_boto_emr.py
@@ -0,0 +1,203 @@
+# Copyright (c) 2010 Spotify AB
+# Copyright (c) 2010 Jeremy Thurgood <firxen+boto@gmail.com>
+# Copyright (c) 2010-2011 Yelp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Code from boto 2.35.0 to support "cluster" EMR API calls"""
+import boto.utils
+from boto.emr.emrobject import EmrObject
+from boto.resultset import ResultSet
+
+
+# from boto/emr/connection.py (these are EMRConnection methods in boto)
+
+def describe_cluster(emr_conn, cluster_id):
+    """
+    Describes an Elastic MapReduce cluster
+
+    :type cluster_id: str
+    :param cluster_id: The cluster id of interest
+    """
+    params = {
+        'ClusterId': cluster_id
+    }
+    return emr_conn.get_object('DescribeCluster', params, Cluster)
+
+
+def list_clusters(emr_conn, created_after=None, created_before=None,
+                  cluster_states=None, marker=None):
+    """
+    List Elastic MapReduce clusters with optional filtering
+
+    :type created_after: datetime
+    :param created_after: Bound on cluster creation time
+    :type created_before: datetime
+    :param created_before: Bound on cluster creation time
+    :type cluster_states: list
+    :param cluster_states: Bound on cluster states
+    :type marker: str
+    :param marker: Pagination marker
+    """
+    params = {}
+    if created_after:
+        params['CreatedAfter'] = created_after.strftime(
+            boto.utils.ISO8601)
+    if created_before:
+        params['CreatedBefore'] = created_before.strftime(
+            boto.utils.ISO8601)
+    if marker:
+        params['Marker'] = marker
+
+    if cluster_states:
+        emr_conn.build_list_params(params, cluster_states, 'ClusterStates.member')
+
+    return emr_conn.get_object('ListClusters', params, ClusterSummaryList)
+
+
+# from boto/emr/emrobject.py
+
+class Application(EmrObject):
+    Fields = set([
+        'Name',
+        'Version',
+        'Args',
+        'AdditionalInfo'
+    ])
+
+
+class Cluster(EmrObject):
+    Fields = set([
+        'Id',
+        'Name',
+        'LogUri',
+        'RequestedAmiVersion',
+        'RunningAmiVersion',
+        'AutoTerminate',
+        'TerminationProtected',
+        'VisibleToAllUsers',
+        'MasterPublicDnsName',
+        'NormalizedInstanceHours',
+        'ServiceRole'
+    ])
+
+    def __init__(self, connection=None):
+        self.connection = connection
+        self.status = None
+        self.ec2instanceattributes = None
+        self.applications = None
+        self.tags = None
+
+    def startElement(self, name, attrs, connection):
+        if name == 'Status':
+            self.status = ClusterStatus()
+            return self.status
+        elif name == 'Ec2InstanceAttributes':
+            self.ec2instanceattributes = Ec2InstanceAttributes()
+            return self.ec2instanceattributes
+        elif name == 'Applications':
+            self.applications = ResultSet([('member', Application)])
+            return self.applications
+        elif name == 'Tags':
+            self.tags = ResultSet([('member', KeyValue)])
+            return self.tags
+        else:
+            return None
+
+
+class ClusterStateChangeReason(EmrObject):
+    Fields = set([
+        'Code',
+        'Message'
+    ])
+
+
+class ClusterStatus(EmrObject):
+    Fields = set([
+        'State',
+        'StateChangeReason',
+        'Timeline'
+    ])
+
+    def __init__(self, connection=None):
+        self.connection = connection
+        self.timeline = None
+
+    def startElement(self, name, attrs, connection):
+        if name == 'Timeline':
+            self.timeline = ClusterTimeline()
+            return self.timeline
+        elif name == 'StateChangeReason':
+            self.statechangereason = ClusterStateChangeReason()
+            return self.statechangereason
+        else:
+            return None
+
+
+class ClusterSummary(EmrObject):
+    Fields = set([
+        'Id',
+        'Name',
+        'NormalizedInstanceHours'
+    ])
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.status = None
+
+    def startElement(self, name, attrs, connection):
+        if name == 'Status':
+            self.status = ClusterStatus()
+            return self.status
+        else:
+            return None
+
+
+class ClusterSummaryList(EmrObject):
+    Fields = set([
+        'Marker'
+    ])
+
+    def __init__(self, connection):
+        self.connection = connection
+        self.clusters = None
+
+    def startElement(self, name, attrs, connection):
+        if name == 'Clusters':
+            self.clusters = ResultSet([('member', ClusterSummary)])
+            return self.clusters
+        else:
+            return None
+
+
+class ClusterTimeline(EmrObject):
+    Fields = set([
+        'CreationDateTime',
+        'ReadyDateTime',
+        'EndDateTime'
+    ])
+
+
+class Ec2InstanceAttributes(EmrObject):
+    Fields = set([
+        'Ec2KeyName',
+        'Ec2SubnetId',
+        'Ec2AvailabilityZone',
+        'IamInstanceProfile'
+    ])
+
+
+class KeyValue(EmrObject):
+    Fields = set([
+        'Key',
+        'Value',
+    ])

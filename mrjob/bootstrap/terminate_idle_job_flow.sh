4e9e842c3e30d8d7d25e54d2ed0870e674e4273a
David Marin <dm@davidmarin.org>
2015-10-16 15:40:15 -0700

    update copyright notice

--- mrjob/bootstrap/terminate_idle_job_flow.sh
+++ mrjob/bootstrap/terminate_idle_job_flow.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Copyright 2013 Lyft
+# Copyright 2014 Alex Konradi
+# Copyright 2015 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

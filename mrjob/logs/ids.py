05cf162fcabb6b3b99a656511299bbcf89bdc759
David Marin <dm@davidmarin.org>
2016-01-29 17:13:40 -0800

    now combining the power of container and attempt IDs!!

--- mrjob/logs/ids.py
+++ mrjob/logs/ids.py
@@ -17,43 +17,59 @@
 
 # TODO: test these!
 
-def _sort_by_recency(ds):
+def _sort_by_recency(ds, container_to_attempt_id=None):
     """Sort the given list/sequence of dicts containing IDs so that the
     most recent ones come first (e.g. to find the best error, or the best
     log file to look for an error in).
     """
-    return sorted(ds, key=_time_sort_key, reverse=True)
+    sort_key=_make_time_sort_key(container_to_attempt_id)
+    return sorted(ds, key=sort_key, reverse=True)
 
 
-def _time_sort_key(d):
+def _make_time_sort_key(container_to_attempt_id=None):
     """Sort key to sort the given dictionaries containing IDs roughly by time
     (earliest first).
 
     We consider higher attempt_nums "later" than higher task_nums (of the
     same step type) because fatal errors usually occur on the final
     attempt of a task.
+
+    If we can, we convert (YARN) container IDs to attempt IDs. Unconverted
+    container IDs are considered more "recent" than any task/attempt ID
+    (these usually come from task logs).
     """
-    # break ID like
-    # {application,attempt,task,job}_201601081945_0005[_m[_000005[_0]]] into
-    # its component parts
-    attempt_parts = (d.get('attempt_id') or d.get('task_id')
-                     or d.get('job_id') or d.get('application_id')
-                     or '').split('_')
+    container_to_attempt_id = container_to_attempt_id or {}
 
-    timestamp_and_step = '_'.join(attempt_parts[1:3])
-    task_type = '_'.join(attempt_parts[3:4])
-    task_num = '_'.join(attempt_parts[4:5])
-    attempt_num = '_'.join(attempt_parts[5:6])
+    def sort_key(d):
+        container_id = d.get('container_id') or ''
 
-    # numbers are 0-padded, so no need to convert anything to int
-    # also, 'm' (task type in attempt ID) sorts before 'r', which is
-    # what we want
-    return (
-        timestamp_and_step,
-        d.get('container_id') or '',
-        task_type,
-        attempt_num,
-        task_num)
+        inferred_attempt_id = container_to_attempt_id.get(container_id) or ''
+        if inferred_attempt_id:
+            container_id = ''
+
+        # break ID like
+        # {application,attempt,task,job}_201601081945_0005[_m[_000005[_0]]]
+        # into its component parts
+        attempt_parts = (d.get('attempt_id') or inferred_attempt_id or
+                         d.get('task_id') or d.get('job_id') or
+                         d.get('application_id') or '').split('_')
+
+        timestamp_and_step = '_'.join(attempt_parts[1:3])
+        task_type = '_'.join(attempt_parts[3:4])
+        task_num = '_'.join(attempt_parts[4:5])
+        attempt_num = '_'.join(attempt_parts[5:6])
+
+        # numbers are 0-padded, so no need to convert anything to int
+        # also, 'm' (task type in attempt ID) sorts before 'r', which is
+        # what we want
+        return (
+            timestamp_and_step,
+            container_id,
+            task_type,
+            attempt_num,
+            task_num)
+
+    return sort_key
 
 
 def _add_implied_task_id(d):

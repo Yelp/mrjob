b6488b3e60cf6c44f8ad88f74ac12220b16603a4
David Marin <dm@davidmarin.org>
2016-03-06 14:16:04 -0800

    hid some other functions and constants in mrjob.parse

--- mrjob/local.py
+++ mrjob/local.py
@@ -20,7 +20,7 @@ from subprocess import Popen
 from subprocess import PIPE
 
 from mrjob.logs.counters import _format_counters
-from mrjob.parse import find_python_traceback
+from mrjob.parse import _find_python_traceback
 from mrjob.parse import parse_mr_job_stderr
 from mrjob.py2 import string_types
 from mrjob.sim import SimMRJobRunner
@@ -242,7 +242,7 @@ class LocalMRJobRunner(SimMRJobRunner):
 
         stderr_lines = self._process_stderr_from_script(
             proc.stderr, step_num=step_num)
-        tb_lines = find_python_traceback(stderr_lines)
+        tb_lines = _find_python_traceback(stderr_lines)
 
         # proc.stdout isn't always defined
         if proc.stdout:
--- tests/mockboto.py
+++ tests/mockboto.py
@@ -38,9 +38,9 @@ from mrjob.compat import map_version
 from mrjob.compat import version_gte
 from mrjob.conf import combine_values
 from mrjob.emr import EMRJobRunner
+from mrjob.parse import _RFC1123
 from mrjob.parse import is_s3_uri
 from mrjob.parse import parse_s3_uri
-from mrjob.parse import RFC1123
 from mrjob.ssh import SSH_LOG_ROOT
 
 from tests.mockssh import create_mock_ssh_script
@@ -568,7 +568,7 @@ def to_rfc1123(when):
     # AWS sends us a time zone in all cases, but in Python it's more
     # annoying to figure out time zones, so just fake it.
     assert when.tzinfo is None
-    return when.strftime(RFC1123) + 'GMT'
+    return when.strftime(_RFC1123) + 'GMT'
 
 
 class MockEmrConnection(object):

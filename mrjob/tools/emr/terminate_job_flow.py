0a8c6a3de2b1176ad4f3f99ef014845eb77a9adf
David Marin <dm@davidmarin.org>
2016-05-20 15:56:21 -0700

    fixed main() in deprecated tools modules (fixes #1312)

--- mrjob/tools/emr/terminate_job_flow.py
+++ mrjob/tools/emr/terminate_job_flow.py
@@ -19,7 +19,7 @@ from sys import stderr
 from .terminate_cluster import main as real_main
 
 
-def main(args):
+def main(args=None):
     print(
         'terminate-job-flow is a deprecated alias for'
         ' terminate-cluster and will be removed in v0.6.0',

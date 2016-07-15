a5980b0f97607747e3215428449439e66aefef65
David Marin <dm@davidmarin.org>
2016-07-15 11:21:44 -0700

    Configurations: more fun with EMR API's largely obsolete XML interface

--- mrjob/patched_boto.py
+++ mrjob/patched_boto.py
@@ -90,8 +90,16 @@ class _Configuration(EmrObject):
             self.configurations = ResultSet([('member', _Configuration)])
             return self.configurations
         elif name == 'Properties':
-            self.properties = ResultSet([('member', KeyValue)])
+            self.properties = ResultSet([('entry', _KeyValueEntry)])
             return self.properties
+
+
+class _KeyValueEntry(EmrObject):
+    """Like KeyValue, but used for Properties in _Configurations."""
+    Fields = set([
+        'key',
+        'value',
+    ])
 
 
 class _PatchedCluster(Cluster):

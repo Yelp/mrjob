2a8608d90d317128e9697b747b170412176da885
David Marin <dm@davidmarin.org>
2016-12-20 17:35:43 -0800

    not going to need tar_and_gz() anymore

--- mrjob/util.py
+++ mrjob/util.py
@@ -621,6 +621,8 @@ def tar_and_gzip(dir, out_path, filter=None, prefix=''):
     :param prefix: subdirectory inside the tarball to put everything into (e.g.
                    ``'mrjob'``)
     """
+    log.warn('tar_and_gzip() is deprecated and will be removed in v0.6.0')
+
     if not os.path.isdir(dir):
         raise IOError('Not a directory: %r' % (dir,))
 

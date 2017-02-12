de68b3bd88520113e8090f33a43f1dcc1a6e9ee2
Paul Paczuski <pavopax@gmail.com>
2017-02-11 21:39:47 -0800

    [examples] fix typo

--- mrjob/examples/mr_u_word_freq_count.py
+++ mrjob/examples/mr_u_word_freq_count.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The classic MapReduce job, but only for words containing "v"
+"""The classic MapReduce job, but only for words containing "u"
 """
 from mrjob.job import MRJob
 import re

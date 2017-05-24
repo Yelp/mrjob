329cfafeb023a2b1a288e9293fce1e604edee581
Dave Marin <dave@davemarin.com>
2017-05-24 13:01:30 -0700

    always add idle termination script to persistent/pooled clusters

--- mrjob/emr.py
+++ mrjob/emr.py
@@ -322,7 +322,7 @@ class EMRRunnerOptionStore(RunnerOptionStore):
             'bootstrap_python': None,
             'check_cluster_every': 30,
             'cleanup_on_failure': ['JOB'],
-            'max_hours_idle': 1,
+            'max_hours_idle': 1.0,
             'mins_to_end_of_hour': 5.0,
             'num_core_instances': 0,
             'num_task_instances': 0,
@@ -806,8 +806,7 @@ class EMRJobRunner(MRJobRunner, LogInterpretationMixin):
             self._upload_mgr.add(bootstrap_action['path'])
 
         # Add max-hours-idle script if we need it
-        if (self._opts['max_hours_idle'] and
-                (persistent or self._opts['pool_clusters'])):
+        if persistent or self._opts['pool_clusters']:
             self._upload_mgr.add(_MAX_HOURS_IDLE_BOOTSTRAP_ACTION_PATH)
 
     def _add_master_node_setup_files_for_upload(self):
@@ -1341,19 +1340,18 @@ class EMRJobRunner(MRJobRunner, LogInterpretationMixin):
         if persistent or self._opts['pool_clusters']:
             Instances['KeepJobFlowAliveWhenNoSteps'] = True
 
-            # only use idle termination script on persistent clusters
+            # use idle termination script on persistent clusters
             # add it last, so that we don't count bootstrapping as idle time
-            if self._opts['max_hours_idle']:
-                uri = self._upload_mgr.uri(
-                    _MAX_HOURS_IDLE_BOOTSTRAP_ACTION_PATH)
-                # script takes args in (integer) seconds
-                ba_args = [str(int(self._opts['max_hours_idle'] * 3600)),
-                           str(int(self._opts['mins_to_end_of_hour'] * 60))]
-                BootstrapActions.append(dict(
-                    Name='idle timeout',
-                    ScriptBootstrapAction=dict(
-                        Path=uri,
-                        Args=ba_args)))
+            uri = self._upload_mgr.uri(
+                _MAX_HOURS_IDLE_BOOTSTRAP_ACTION_PATH)
+            # script takes args in (integer) seconds
+            ba_args = [str(int(self._opts['max_hours_idle'] * 3600)),
+                       str(int(self._opts['mins_to_end_of_hour'] * 60))]
+            BootstrapActions.append(dict(
+                Name='idle timeout',
+                ScriptBootstrapAction=dict(
+                    Path=uri,
+                    Args=ba_args)))
 
         if self._opts['ec2_key_pair']:
             Instances['Ec2KeyName'] = self._opts['ec2_key_pair']

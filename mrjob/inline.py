56c2358cab90388d111ec3bf4e0979c087371598
Dave Marin <dave@davemarin.com>
2017-06-26 17:12:05 -0700

    somehow never checked this in!

---
+++ mrjob/inline.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# Copyright 2011 Matthew Tai and Yelp
+# Copyright 2012-2016 Yelp and Contributors
+# Copyright 2017 Yelp
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Run an MRJob inline by running all mappers and reducers through the same
+process. Useful for debugging."""
+import logging
+import os
+
+from mrjob.job import MRJob
+from mrjob.options import _allowed_keys
+from mrjob.options import _combiners
+from mrjob.options import _deprecated_aliases
+#from mrjob.parse import parse_mr_job_stderr
+from mrjob.runner import RunnerOptionStore
+from mrjob.sim import SimMRJobRunner
+from mrjob.util import save_current_environment
+from mrjob.util import save_cwd
+
+log = logging.getLogger(__name__)
+
+
+class InlineRunnerOptionStore(RunnerOptionStore):
+    ALLOWED_KEYS = _allowed_keys('inline')
+    COMBINERS = _combiners('inline')
+    DEPRECATED_ALIASES = _deprecated_aliases('inline')
+
+
+class InlineMRJobRunner(SimMRJobRunner):
+    """Runs an :py:class:`~mrjob.job.MRJob` in the same process, so it's easy
+    to attach a debugger.
+
+    This is the default way to run jobs (we assume you'll spend some time
+    debugging your job before you're ready to run it on EMR or Hadoop).
+
+    Unlike other runners, ``InlineMRJobRunner``\ 's ``run()`` method
+    raises the actual exception that caused a step to fail (rather than
+    :py:class:`~mrjob.step.StepFailedException`).
+
+    To more accurately simulate your environment prior to running on
+    Hadoop/EMR, use ``-r local`` (see
+    :py:class:`~mrjob.local.LocalMRJobRunner`).
+    """
+    OPTION_STORE_CLASS = InlineRunnerOptionStore
+
+    alias = 'inline'
+
+    def __init__(self, mrjob_cls=None, **kwargs):
+        """:py:class:`~mrjob.inline.InlineMRJobRunner` takes the same keyword
+        args as :py:class:`~mrjob.runner.MRJobRunner`. However, please note
+        that
+        *hadoop_input_format*, *hadoop_output_format*, and *partitioner*
+        are ignored
+        because they require Java. If you need to test these, consider
+        starting up a standalone Hadoop instance and running your job with
+        ``-r hadoop``."""
+        super(InlineMRJobRunner, self).__init__(**kwargs)
+        if not (mrjob_cls is None or issubclass(mrjob_cls, MRJob)):
+            raise TypeError
+
+        self._mrjob_cls = mrjob_cls
+
+    def _invoke_task(
+            self, task_type, step_num, stdin, stdout, stderr, wd, env):
+        """Just run tasks in the same process."""
+
+        with save_current_environment(), save_cwd():
+            os.environ.update(env)
+            os.chdir(wd)
+
+            task = self._mrjob_cls(
+                args=self._args_for_task(step_num, task_type))
+            task.sandbox(stdin=stdin, stdout=stdout, stderr=stderr)
+
+            task.execute()
+
+    def _run_multiple(self, tasks, num_processes=None):
+        """Just run the tasks inline, one at a time.
+        """
+        for func, args, kwargs in tasks:
+            func(*args, **kwargs)
+
+    # avoid subprocesses
+
+    def _create_setup_wrapper_script(self, local=False):
+        # inline mode doesn't need this (no subprocesses)
+        pass
+
+    def _get_steps(self):
+        """Redefine this so that we can get step descriptions without
+        calling a subprocess."""
+        if self._steps is None:
+            job_args = ['--steps'] + self._mr_job_extra_args(local=True)
+            self._steps = self._mrjob_cls(args=job_args)._steps_desc()
+
+        return self._steps

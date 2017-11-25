7a3d28001d8d17a5082e4663c4476931ca383938
Dave Marin <dave@davemarin.com>
2017-11-25 15:23:48 -0800

    added steps for diagnose utility

--- tests/tools/test_diagnose.py
+++ tests/tools/test_diagnose.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 """Test the diagnose script"""
 
-from mrjob.tools.diagnose import main
+from mrjob.tools.diagnose import diagnose_main
 
 from tests.mock_boto3 import MockBoto3TestCase
 from tests.py2 import patch
@@ -31,11 +31,11 @@ class StepPickingTestCase(MockBoto3TestCase):
         self.log = self.start(
             patch('mrjob.tools.diagnose.log'))
 
-    def test_single_failed_job(self):
+    def test_single_failed_step(self):
         self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
             _Steps=[
                 dict(
-                    Config=dict(Args=['python', 'mr_bad.py', '--mapper']),
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
                     Id='s-MOCKSTEP0',
                     Name='mr_failure',
                     Status=dict(State='FAILED'),
@@ -43,15 +43,120 @@ class StepPickingTestCase(MockBoto3TestCase):
             ]
         )
 
-        self.assertRaises(StopIteration, main, ['j-MOCKCLUSTER0'])
+        self.assertRaises(StopIteration, diagnose_main, ['j-MOCKCLUSTER0'])
 
         self.pick_error.assert_called_once_with(
             dict(step_id='s-MOCKSTEP0'), 'streaming')
 
+    def test_failed_step_followed_by_successful_step(self):
+        self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
+            _Steps=[
+                dict(
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
+                    Id='s-MOCKSTEP0',
+                    Name='mr_failure',
+                    Status=dict(State='FAILED'),
+                ),
+                dict(
+                    Config=dict(Args=['python', 'mr_success.py', '--mapper']),
+                    Id='s-MOCKSTEP1',
+                    Name='mr_success',
+                    Status=dict(State='COMPLETED'),
+                ),
+            ]
+        )
 
+        self.assertRaises(StopIteration, diagnose_main, ['j-MOCKCLUSTER0'])
 
-# to test:
-#
-# - detect failed step in multi-step job
-# - detect spark and spark-script steps
-# - handle
+        self.pick_error.assert_called_once_with(
+            dict(step_id='s-MOCKSTEP0'), 'streaming')
+
+    def test_two_failed_steps(self):
+        self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
+            _Steps=[
+                dict(
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
+                    Id='s-MOCKSTEP0',
+                    Name='mr_failure',
+                    Status=dict(State='FAILED'),
+                ),
+                dict(
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
+                    Id='s-MOCKSTEP1',
+                    Name='mr_failure',
+                    Status=dict(State='FAILED'),
+                ),
+            ]
+        )
+
+        self.assertRaises(StopIteration, diagnose_main, ['j-MOCKCLUSTER0'])
+
+        self.pick_error.assert_called_once_with(
+            dict(step_id='s-MOCKSTEP1'), 'streaming')
+
+    def test_no_failed_steps(self):
+        self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
+            _Steps=[
+                dict(
+                    Config=dict(Args=['python', 'mr_success.py', '--mapper']),
+                    Id='s-MOCKSTEP0',
+                    Name='mr_success',
+                    Status=dict(State='COMPLETED'),
+                ),
+            ]
+        )
+
+        self.assertRaises(SystemExit, diagnose_main, ['j-MOCKCLUSTER0'])
+
+        self.assertTrue(self.log.error.called)
+        self.assertFalse(self.pick_error.called)
+
+    def test_failed_spark_step(self):
+        self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
+            _Steps=[
+                dict(
+                    Config=dict(Args=[
+                        'custom-spark-submit',
+                        '--master',
+                        'yarn',
+                        '--deploy-mode',
+                        'cluster',
+                        'custom-spark.jar',
+                        'more-args',
+                    ]),
+                    Id='s-MOCKSTEP0',
+                    Name='mr_spark_jar',
+                    Status=dict(State='FAILED'),
+                ),
+            ]
+        )
+
+        self.assertRaises(StopIteration, diagnose_main, ['j-MOCKCLUSTER0'])
+
+        # doesn't distinguish between different kinds of spark steps
+        self.pick_error.assert_called_once_with(
+            dict(step_id='s-MOCKSTEP0'), 'spark')
+
+    def test_pick_step_id(self):
+        self.mock_emr_clusters['j-MOCKCLUSTER0'] = dict(
+            _Steps=[
+                dict(
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
+                    Id='s-MOCKSTEP0',
+                    Name='mr_failure',
+                    Status=dict(State='FAILED'),
+                ),
+                dict(
+                    Config=dict(Args=['python', 'mr_failure.py', '--mapper']),
+                    Id='s-MOCKSTEP1',
+                    Name='mr_failure',
+                    Status=dict(State='FAILED'),
+                ),
+            ]
+        )
+
+        self.assertRaises(StopIteration, diagnose_main,
+                          ['j-MOCKCLUSTER0', '--step-id', 's-MOCKSTEP0'])
+
+        self.pick_error.assert_called_once_with(
+            dict(step_id='s-MOCKSTEP0'), 'streaming')

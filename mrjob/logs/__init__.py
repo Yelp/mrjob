9174365a36eaee458c89a1d3346b87af35f8814f
Dave Marin <dave@davemarin.com>
2017-11-27 13:08:08 -0800

    updated copyright notices

--- mrjob/logs/__init__.py
+++ mrjob/logs/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015-2016 Yelp and Contributors
+# Copyright 2017 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

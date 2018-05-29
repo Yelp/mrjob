df8b4d07f30f06c3a186223555903ca1645cbb47
Dave Marin <dave@davemarin.com>
2018-05-29 13:40:09 -0700

    updated copyright notices for non-test code

--- mrjob/examples/mr_jar_step_example.py
+++ mrjob/examples/mr_jar_step_example.py
@@ -1,6 +1,6 @@
 # Copyright 2013 David Marin
 # Copyright 2016 Yelp
-# Copyright 2018 Google Inc.
+# Copyright 2018 Yelp and Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

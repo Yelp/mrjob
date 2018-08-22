5d40bc6e9d5500df723514be4d827ab39645d9c1
Dave Marin <dave@davemarin.com>
2018-08-22 14:46:19 -0700

    started on make_custom_emr_ami()

--- mrjob/ami.py
+++ mrjob/ami.py
@@ -13,13 +13,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Utilities for creating custom AMIs."""
+import logging
 import re
 
+
+log = logging.getLogger(__name__)
+
 # used to match the name of AMIs that we want to use on EMR
-EMR_BASE_AMI_NAME_RE = re.compile(r'^amzn-ami-hvm-[\d\.]*-x86_64-ebs$')
+_EMR_BASE_AMI_NAME_RE = re.compile(r'^amzn-ami-hvm-[\d\.]*-x86_64-ebs$')
+
+# default instance type for building AMI images
+_DEFAULT_AMI_INSTANCE_TYPE = 'm1.medium'
 
 
-def get_latest_emr_base_ami(ec2_client):
+
+def _make_custom_emr_ami_image(ec2_client, sh_content, **params):
+    """Make a custom AMI image. This will launch an EC2 image that runs
+    commands and then shuts itself down.
+
+    :param ec2_client: a boto3 EC2 client
+    :param sh_content: a list of lines for a shell script
+    :param params: a dictionary of request parameters for the RunInstances
+                   API. Some that are most relevant:
+
+    :param ImageId: the base AMI (defaults to latest Amazon Linux)
+    :param InstanceType: what instance to build the AMI on (defaults to
+                         ``m1.medium``)
+    :param KeyName: name of SSH key to use (default is None, i.e. no SSH)
+    :param Placement: availablility zone etc. (default is None)
+    :param SubnetId: subnet to launch in (default is None)
+    """
+    pass
+
+#if not base_ami_id:
+#        log.info('looking up latest Amazon Linux AMI...')
+#        image = get_latest_emr_base_ami(ec2_client)
+#        log.info('  found %s (%s)' % (image['ImageId'], image['Description']))
+
+#    if not instance_type:
+#        instance_type = _DEFAULT_AMI_INSTANCE_TYPE
+
+def _get_latest_emr_base_ami(ec2_client):
     """Fetch the latest Amazon Linux AMI image that's usable as a base
     image for EMR. This can take several seconds.
 
@@ -51,7 +85,7 @@ def get_latest_emr_base_ami(ec2_client):
 
     # perform further filtering by name
     images = [img for img in images
-              if EMR_BASE_AMI_NAME_RE.match(img['Name'])]
+              if _EMR_BASE_AMI_NAME_RE.match(img['Name'])]
 
     # sort by creation datetime
     images.sort(key=lambda img: img['CreationDate'])

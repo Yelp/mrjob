53a70cb75988b46ba8cf018b00e59a3ac43f982e
Dave Marin <dave@davemarin.com>
2018-09-04 12:00:03 -0700

    double-check that our EMR base images only have a single volume

--- mrjob/ami.py
+++ mrjob/ami.py
@@ -56,13 +56,16 @@ def describe_base_emr_images(ec2_client):
         ],
     )['Images']
 
-    # TODO: only allow single EBS volume
-
-    # perform further filtering by name
+    # perform further filtering by name to pick out Amazon Linux
     images = [img for img in images
               if _EMR_BASE_AMI_NAME_RE.match(img['Name'])]
 
-    # sort by creation datetime
+    # filter out any images that have multiple volumes
+    # (this is implied by the naming convention, but just in case)
+    images = [img for img in images
+              if len(img['BlockDeviceMappings']) == 1]
+
+    # put most recent images first
     images.sort(key=lambda img: img['CreationDate'], reverse=True)
 
     return images

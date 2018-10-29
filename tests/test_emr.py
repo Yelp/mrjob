0e028ea55d5f5034177b8fa568376970c523e584
Dave Marin <dave@davemarin.com>
2018-10-29 16:12:00 -0700

    fix name of log.warning in test

--- tests/test_emr.py
+++ tests/test_emr.py
@@ -3057,7 +3057,7 @@ class BootstrapPythonTestCase(MockBoto3TestCase):
 
                 if not PY2:
                     warnings = ''.join(
-                        a[0] + '\n' for a, kw in log.warnings.call_args_list)
+                        a[0] + '\n' for a, kw in log.warning.call_args_list)
                     self.assertIn('will probably not work', warnings)
 
     def _assert_never_installs_python3(self, *args):

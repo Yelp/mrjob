d990e539814ce554a196246cb3b6aeb32c561ea1
Dave Marin <dave@davemarin.com>
2018-11-05 12:17:06 -0800

    updated copyright notice on tests

--- tests/test_py2.py
+++ tests/test_py2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 # Copyright 2015 Yelp
 # Copyright 2017 Yelp
+# Copyright 2018 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 # Copyright 2009-2015 Yelp and Contributors
 # Copyright 2016-2017 Yelp
+# Copyright 2018 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

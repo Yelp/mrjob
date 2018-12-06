bed967476a93019ec0e1b34e2e49e0dcbb364f4d
Dave Marin <dave@davemarin.com>
2018-12-06 14:14:43 -0800

    made basic help message match spark-submit's help message

--- mrjob/tools/spark_submit.py
+++ mrjob/tools/spark_submit.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 import os
 import sys
 from argparse import ArgumentParser
+from argparse import SUPPRESS
 from logging import getLogger
 
 from mrjob.conf import combine_lists
@@ -35,7 +36,7 @@ log = getLogger(__name__)
 _USAGE = ('%(prog)s spark-submit [-r <runner>] [options]'
           ' <python file | app jar> [app arguments]')
 
-_DESCRIPTION = 'Submit a spark job using mrjob runners'
+_DESCRIPTION = 'Submit a spark job locally or in the cloud'
 
 _BASIC_HELP_EPILOG = (
     'To see help for a specific runner, use --help -r <runner name>')
@@ -57,9 +58,16 @@ _SPARK_RUNNERS = ('emr', 'hadoop')
 _DEFAULT_RUNNER = 'hadoop'
 
 
+# our mostly similar version of spark-submit's args, arranged in to groups
+# for the --help message. Differences:
+#
+# spark_master (--master) is in its own "Hadoop runner only" group
+# added upload_dirs (--dirs) which is similar to --archive
+#
+# --runner and other basic options are patched into the first ("None")
+# argument group in _make_basic_help_parser(), below
 _SPARK_SUBMIT_ARG_GROUPS = [
     (None, [
-        'spark_master',
         'spark_deploy_mode',
         'main_class',
         'name',
@@ -78,6 +86,9 @@ _SPARK_SUBMIT_ARG_GROUPS = [
         'executor_memory',
         'proxy_user',
     ]),
+    ('Hadoop runner only', [
+        'spark_master',
+    ]),
     ('Cluster deploy mode only', [
         'driver_cores',
     ]),
@@ -95,10 +106,74 @@ _SPARK_SUBMIT_ARG_GROUPS = [
         'queue_name',
         'num_executors',
         'upload_archives',
+        'upload_dirs',
         'principal',
         'keytab',
     ]),
 ]
+
+# lightly modified versions of help messages from spark-submit
+_SPARK_SUBMIT_ARG_HELP = dict(
+    driver_class_path=('Extra class path entries to pass to the driver. Note'
+                       ' that jars added with --jars are automatically'
+                       ' included in the classpath.'),
+    driver_cores='Number of cores used by the driver (Default: 1).',
+    driver_java_options='Extra Java options to pass to the driver.',
+    driver_library_path='Extra library path entries to pass to the driver.',
+    driver_memory='Memory for driver (e.g. 1000M, 2G) (Default: 1024M).',
+    exclude_packages=('Comma-separated list of groupId:artifactId, to exclude'
+                      ' while resolving the dependencies provided in'
+                      ' --packages to avoid dependency conflicts.'),
+    executor_cores=('Number of cores per executor. (Default: 1 in YARN mode,'
+                    ' or all available cores on the worker in standalone'
+                    ' mode)'),
+    executor_memory='Memory per executor (e.g. 1000M, 2G) (Default: 1G).',
+    jobconf='Arbitrary Spark configuration property.',
+    keytab=('The full path to the file that contains the keytab for the'
+            ' principal specified above. This keytab will be copied to'
+            ' the node running the Application Master via the Secure'
+            ' Distributed Cache, for renewing the login tickets and the'
+            ' delegation tokens periodically.'),
+    libjars=('Comma-separated list of jars to include on the driver'
+             'and executor classpaths.'),
+    main_class="Your application's main class (for Java / Scala apps).",
+    name='The name of your application.',
+    num_executors=('Number of executors to launch (Default: 2).'
+                   ' If dynamic allocation is enabled, the initial number of'
+                   ' executors will be at least NUM.'),
+    packages=('Comma-separated list of maven coordinates of jars to include'
+              ' on the driver and executor classpaths. Will search the local'
+              ' maven repo, then maven central and any additional remote'
+              ' repositories given by --repositories. The format for the'
+              ' coordinates should be groupId:artifactId:version.'),
+    principal=('Principal to be used to login to KDC, while running on'
+               'secure HDFS.'),
+    properties_file=('Path to a file from which to load extra properties. If'
+                     ' not specified, this will look for'
+                     ' conf/spark-defaults.conf.'),
+    proxy_user=('User to impersonate when submitting the application.'
+                ' This argument does not work with --principal / --keytab.'),
+    py_files=('Comma-separated list of .zip, .egg, or .py files to place'
+              'on the PYTHONPATH for Python apps.'),
+    queue_name='The YARN queue to submit to (Default: "default").',
+    repositories=('Comma-separated list of additional remote repositories to'
+                  ' search for the maven coordinates given with --packages.'),
+    spark_deploy_mode=('Whether to launch the driver program locally'
+                       ' ("client") or on one of the worker machines inside'
+                       ' the cluster ("cluster") (Default: client).'),
+    spark_master=('spark://host:port, mesos://host:port, yarn,'
+                  'k8s://https://host:port, or local (Default: yarn).'),
+    supervise='If given, restarts the driver on failure.',
+    total_executor_cores='Total cores for all executors.',
+    upload_archives=('Comma-separated list of archives to be extracted into'
+                     ' the working directory of each executor.'),
+    upload_dirs=('Comma-separated list of directors to be archived and then'
+                 ' extracted into the working directory of each executor.'),
+    upload_files=('Comma-separated list of files to be placed in the working'
+                  ' directory of each executor. File paths of these files'
+                  ' in executors can be accessed via'
+                  ' SparkFiles.get(fileName).'),
+)
 
 _SPARK_SUBMIT_OPT_NAMES = {
     opt_name for _, opt_names in _SPARK_SUBMIT_ARG_GROUPS
@@ -132,7 +207,13 @@ _SPARK_SUBMIT_SWITCHES = dict(
     supervise='--supervise',
     total_executor_cores='--total-executor-cores',
     upload_archives='--archives',
+    upload_dirs='--dirs',
     upload_files='--files',
+)
+
+# things that are different about specific spark submit args
+_SPARK_SUBMIT_ARG_KWARGS = dict(
+    supervise=dict(action='store_true'),
 )
 
 # not a runner opt or one that's passed straight through to spark
@@ -142,8 +223,6 @@ _STEP_OPT_NAMES = {'main_class'}
 _SPARK_ARG_OPT_NAMES = (
     set(_SPARK_SUBMIT_SWITCHES) - set(_RUNNER_OPTS) - _STEP_OPT_NAMES)
 
-
-
 _SWITCH_ALIASES = {
     '--master': '--spark-master',
     '--deploy-mode': '--spark-deploy-mode',
@@ -151,10 +230,7 @@ _SWITCH_ALIASES = {
     '--conf': '--jobconf',
 }
 
-
 # these options don't make any sense with Spark scripts
-
-# TODO: suppress this in --help
 _HARD_CODED_OPTS = dict(
     check_input_paths=False,
     output_dir=None,
@@ -165,17 +241,17 @@ def main(cl_args=None):
     parser = _make_arg_parser()
     options = parser.parse_args(cl_args)
 
+    runner_alias = options.runner or _DEFAULT_RUNNER
+    runner_class = _runner_class(runner_alias)
+
     if options.help or not options.script_or_jar:
-        _print_help(options)
+        _print_help(options, runner_class)
         sys.exit(0)
 
     MRJob.set_up_logging(
         quiet=options.quiet,
         verbose=options.verbose,
     )
-
-    runner_alias = options.runner or _DEFAULT_RUNNER
-    runner_class = _runner_class(runner_alias)
 
     kwargs = _get_runner_opt_kwargs(options, runner_class)
     kwargs['input_paths'] = [os.devnull]
@@ -252,6 +328,9 @@ def _add_spark_submit_arg(parser, opt_name):
             if opt_alias in opt_strings:
                 kwargs.update(opt_kwargs)
 
+    kwargs['help'] = _SPARK_SUBMIT_ARG_HELP[opt_name]
+    kwargs.update(_SPARK_SUBMIT_ARG_KWARGS.get(opt_name) or {})
+
     parser.add_argument(opt_string, **kwargs)
 
 
@@ -302,56 +381,59 @@ def _add_deprecated_arg(parser):
         help='include help for deprecated options')
 
 
-def _print_help(options):
-    if options.runner:
-        _print_help_for_runner(options.runner, options.deprecated)
+def _print_help(options, runner_class):
+    if options.help and options.runner:
+        # if user specifies -r without -h, show basic help
+        _print_help_for_runner(runner_class, options.deprecated)
     else:
         _print_basic_help(options.deprecated)
 
 
-def _print_help_for_runner(runner_alias, include_deprecated=False):
-    # TODO: finish this
-    print('<help for %s runner>' % runner_alias)
+def _print_help_for_runner(runner_class, include_deprecated=False):
+    help_parser = ArgumentParser(usage=SUPPRESS, add_help=False)
+
+    opt_names = runner_class.OPT_NAMES - set(_HARD_CODED_OPTS)
+    _add_runner_args(help_parser, opt_names,
+                     include_deprecated=include_deprecated)
+
+    help_parser.print_help()
 
 
 def _print_basic_help(include_deprecated=False):
-    _make_basic_help_arg_parser(include_deprecated).print_help()
+    _make_basic_help_parser(include_deprecated).print_help()
 
     if not include_deprecated:
         print()
         print(_DEPRECATED_OPT_HELP)
 
 
-def _make_basic_help_arg_parser(include_deprecated=False):
+def _make_basic_help_parser(include_deprecated=False):
     """Make an arg parser that's used only for printing basic help.
 
     This prints help very similar to spark-submit itself. Runner args
     are not included unless they are also spark-submit args (e.g. --py-files)
     """
-    parser = ArgumentParser(usage=_USAGE, description=_DESCRIPTION,
-                            epilog=_BASIC_HELP_EPILOG, add_help=False)
+    help_parser = ArgumentParser(usage=_USAGE, description=_DESCRIPTION,
+                                 epilog=_BASIC_HELP_EPILOG, add_help=False)
 
-    _add_runner_alias_arg(parser)
+    _add_runner_alias_arg(help_parser)
 
     for group_desc, opt_names in _SPARK_SUBMIT_ARG_GROUPS:
         if group_desc is None:
-            parser_or_group = parser
+            parser_or_group = help_parser
         else:
-            parser_or_group = parser.add_argument_group(group_desc)
+            parser_or_group = help_parser.add_argument_group(group_desc)
 
         for opt_name in opt_names:
             _add_spark_submit_arg(parser_or_group, opt_name)
 
         if group_desc is None:
-            _add_basic_args(parser)
-            _add_help_arg(parser)
+            _add_basic_args(help_parser)
+            _add_help_arg(help_parser)
             if include_deprecated:
-                _add_deprecated_arg(parser)
+                _add_deprecated_arg(help_parser)
 
-
-            # TODO: add --deprecated and --help
-
-    return parser
+    return help_parser
 
 
 

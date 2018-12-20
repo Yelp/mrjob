a01660f616ad74d614256f3cf84fc7e09b94f707
Dave Marin <dave@davemarin.com>
2018-12-20 11:58:28 -0800

    tested what happens when no script or steps are passed to the runner

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1018,3 +1018,13 @@ class PassStepsToRunnerTestCase(BasicTestCase):
             self.assertEqual(runner._steps, [])
 
             self.assertRaises(ValueError, runner.run)
+
+    def test_no_script_and_no_steps(self):
+        runner = InlineMRJobRunner()
+
+        self.assertEqual(runner._script_path, None)
+        self.assertEqual(runner._steps, [])
+
+        self.assertRaises(AssertionError, runner.run)
+
+        self.assertFalse(self.log.warning.called)

6cd04d78e2baf28eb724e04596f765a71928cd31
Dave Marin <dave@davemarin.com>
2019-04-21 17:52:49 -0700

    update copyright notice in tests

--- tests/fs/test_s3.py
+++ tests/fs/test_s3.py
@@ -2,6 +2,7 @@
 # Copyright 2015 Yelp
 # Copyright 2017 Yelp and Contributors
 # Copyright 2018 Yelp
+# Copyright 2019 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

7e9cf7f5301c7fd4d189826fd23ab548423745ed
Dave Marin <dave@davemarin.com>
2019-08-28 20:50:57 -0700

    don't use MockGoogleTestCase in Python 3.4

--- tests/spark/test_runner.py
+++ tests/spark/test_runner.py
@@ -18,6 +18,7 @@ from os import listdir
 from os.path import exists
 from os.path import join
 from unittest import skipIf
+import sys
 
 try:
     import pyspark
@@ -43,7 +44,6 @@ from mrjob.util import safeeval
 from mrjob.util import to_lines
 
 from tests.mock_boto3 import MockBoto3TestCase
-from tests.mock_google import MockGoogleTestCase
 from tests.mockhadoop import MockHadoopTestCase
 from tests.mr_count_lines_by_filename import MRCountLinesByFilename
 from tests.mr_doubler import MRDoubler
@@ -63,6 +63,13 @@ from tests.py2 import patch
 from tests.sandbox import SandboxedTestCase
 from tests.sandbox import mrjob_conf_patcher
 from tests.test_bin import _LOCAL_CLUSTER_MASTER
+
+# Google libs don't work in Python 3.4. The tests don't actually use
+# Google Cloud Storage, just mocking out Google out of an abundance of caution
+if sys.version_info[:2] != (3, 4):
+    from tests.mock_google import MockGoogleTestCase
+else:
+    MockGoogleTestCase = object
 
 
 class MockFilesystemsTestCase(

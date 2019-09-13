bca6d4aaf6b9d72903d6f01b43f9cd62530560ae
Dave Marin <dave@davemarin.com>
2019-09-13 15:39:05 -0700

    tested multi-line warnings

--- tests/logs/test_spark.py
+++ tests/logs/test_spark.py
@@ -15,6 +15,9 @@ from tests.sandbox import BasicTestCase
 
 from mrjob.logs.spark import _parse_spark_log
 
+# these are actual error messages from Spark, though the tracebacks
+# in
+
 _SINGLE_LINE_ERROR = """\
 2019-08-29 21:12:43 ERROR SparkHadoopWriter:70 -\
  Task attempt_20190829211242_0004_m_000000_0 aborted."""
@@ -38,6 +41,21 @@ Exception: KABOOM
 .handlePythonException(PythonRunner.scala:452)
 	at java.lang.Thread.run(Thread.java:748)"""
 
+_MULTI_LINE_WARNING = """\
+2019-08-29 21:38:55 INFO  BlockManagerInfo:54 - Added broadcast_0_piece0 in memory on 10.0.1.15:55242 (size: 23.2 KB, free: 366.2 MB)
+2019-08-29 21:38:58 WARN  TaskSetManager:66 - Lost task 1.0 in stage 0.0 (TID 1, 10.0.1.15, executor 0): org.apache.spark.SparkException: Task failed while writing rows
+	at org.apache.spark.internal.io.SparkHadoopWriter$.org$apache$spark$internal$io$SparkHadoopWriter$$executeTask(SparkHadoopWriter.scala:155)
+	at java.lang.Thread.run(Thread.java:748)
+Caused by: org.apache.spark.api.python.PythonException: Traceback (most recent call last):
+  File "/Users/davidmarin/git/venv-2.7-mrjob/lib/python2.7/site-packages/pyspark/python/lib/pyspark.zip/pyspark/worker.py", line 372, in main
+    process()
+  File "/Users/davidmarin/git/mrjob/mrjob/examples/mr_sparkaboom.py", line 28, in kaboom
+    raise Exception('KABOOM')
+Exception: KABOOM
+
+	at org.apache.spark.api.python.BasePythonRunner$ReaderIterator.handlePythonException(PythonRunner.scala:452)
+	at org.apache.spark.internal.io.SparkHadoopWriter$.org$apache$spark$internal$io$SparkHadoopWriter$$executeTask(SparkHadoopWriter.scala:139)
+	... 10 more"""
 
 
 class ParseSparkLogTestCase(BasicTestCase):
@@ -62,6 +80,7 @@ class ParseSparkLogTestCase(BasicTestCase):
         )
 
     def test_ignore_single_line_warning(self):
+        # single-line warnings can be all sorts of irrelevant things
         self.assertEqual(
             _parse_spark_log(_SINGLE_LINE_WARNING.split('\n')),
             {}
@@ -86,4 +105,17 @@ class ParseSparkLogTestCase(BasicTestCase):
     def test_multi_line_warning(self):
         # on the local-cluster master, Python Tracebacks are only available
         # from warnings, not errors
-        pass
+        self.assertEqual(
+            _parse_spark_log(_MULTI_LINE_WARNING.split('\n')),
+                dict(errors=[
+                dict(
+                    spark_error=(
+                        dict(
+                            message=(_MULTI_LINE_WARNING[180:]),
+                            start_line=1,
+                            num_lines=13,
+                        )
+                    )
+                )
+            ])
+        )

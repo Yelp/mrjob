203f916bdb85f119d609a88e03438c66724c0eb6
Dave Marin <dave@davemarin.com>
2019-11-15 18:30:57 -0800

    updated copyright notices

--- mrjob/examples/mr_log_sampler.py
+++ mrjob/examples/mr_log_sampler.py
@@ -1,5 +1,6 @@
 # Copyright 2011 Yelp
 # Copyright 2017 Yelp
+# Copyright 2019 Yelp
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

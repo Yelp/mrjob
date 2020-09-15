d66b887e1c8bb22912007bb3e8fcd346d56a15d4
Coyote Codornices Marin <coyote@coyote.rocks>
2020-09-15 11:53:58 -0700

    updated copyright notices

--- tests/tools/emr/test_create_cluster.py
+++ tests/tools/emr/test_create_cluster.py
@@ -2,6 +2,7 @@
 # Copyright 2015-2017 Yelp
 # Copyright 2018 Yelp and Contributors
 # Copyright 2019 Yelp
+# Copyright 2020 Affirm, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

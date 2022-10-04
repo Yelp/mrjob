29e414da9a35ed6b1d88630fb799de6d1c6e0a35
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:18:46 +0000

    Adding numeric_owner as keyword arguement

--- mrjob/util.py
+++ mrjob/util.py
@@ -362,7 +362,7 @@ def unarchive(archive_path, dest):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(archive, dest)
